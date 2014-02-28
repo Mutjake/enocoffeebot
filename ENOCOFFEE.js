a945c1faa19cdbd5e51b5c831069eb8929aadba9
Juho Myllylahti <mutjake@iki.fi>
2014-02-28 11:28:54 +0200

    Added arvo command.

--- ENOCOFFEE.js
+++ ENOCOFFEE.js
@@ -323,6 +323,12 @@ function onIrcMessage(from, to, message) {
             });
          }
       }
+   } else if (message.substring(0,5) === "!arvo") {
+      if(message.indexOf(" ") !== -1) {
+         var values = message.split(" ").splice(0,1);
+         var result = values[Math.floor(Math.random() * values.length)];
+         ircmsg(result);         
+      }
    }
 }
 
